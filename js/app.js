c8c1f00bb37ee19957bfe91f33c0bd18056671e0
OwethuSotomela <owethusotomela@gmail.com>
2022-05-21 16:38:54 +0200

    Moving functionality to an Alpine file

--- js/app.js
+++ js/app.js
@@ -6,10 +6,22 @@ document.addEventListener('alpine:init', () => {
             },
             open: false,
             userInput: '',
-            todos: [],
+            todos: [
+                {
+                    name: this.userInput,
+                    completed: '',
+                    todo: this.name
+                }
+            ],
             addToDo() {
                 if (this.userInput.trim().length)
                     this.todos.push({ name: this.userInput[0].toUpperCase() + this.userInput.slice(1).toLowerCase(), completed: false }); this.userInput = ''
+            },
+            checkMyBox(todo) {
+                this.todos = this.todos.filter((currentToDo) => currentToDo != todo)
+            },
+            clearAll() {
+
             }
         }
     })
