e592df6bb542f26e21cf519e1b2e03826f284471
OwethuSotomela <owethusotomela@gmail.com>
2022-05-21 12:37:46 +0200

    Restructuring my code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
-document.addEventListener('alpine:init', ()=>{
-    Alpine.data('', ()=>{
+document.addEventListener('alpine:init', () => {
+    Alpine.data('myToDos', () => {
         return {
-            init(){
+            init() {
                 console.log('From scratch')
             },
             open: false,
+            userInput: '',
+            todos: [],
+            addToDo() {
+                if (this.userInput.trim().length)
+                    this.todos.push({ name: this.userInput[0].toUpperCase() + this.userInput.slice(1).toLowerCase(), completed: false }); this.userInput = ''
+            }
         }
     })
 })
