562756f397a935c94ae889e01d4b9789ac050e9a
Owethu Sotomela <owethusotomela@gmail.com>
2024-05-16 06:40:30 +0200

    Enhanced UI

--- js/app.js
+++ js/app.js
@@ -1,28 +1,20 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('myToDos', () => {
         return {
-            init() {
-                console.log('From scratch')
-            },
-            open: false,
             userInput: '',
-            todos: [
-                {
-                    name: this.userInput,
-                    completed: '',
-                    todo: this.name
-                }
-            ],
+            todos: [],
             addToDo() {
-                if (this.userInput.trim().length)
-                    this.todos.push({ name: this.userInput[0].toUpperCase() + this.userInput.slice(1).toLowerCase(), completed: false }); this.userInput = ''
+                if (this.userInput.trim().length) {
+                    this.todos.push({ 
+                        name: this.userInput.charAt(0).toUpperCase() + this.userInput.slice(1), 
+                        completed: false 
+                    });
+                    this.userInput = '';
+                }
             },
             checkMyBox(todo) {
-                this.todos = this.todos.filter((currentToDo) => currentToDo != todo)
-            },
-            clearAll() {
-
+                this.todos = this.todos.filter(t => t !== todo);
             }
-        }
-    })
-})
+        };
+    });
+});
